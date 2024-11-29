593f6ca2fba2c3497d36c8aa887b24a7d5e9096b
hoangduyqsqt <duymapqsqt@gmail.com>
2024-11-29 20:37:44 +0700

    duy

--- src/orders/orders.route.js
+++ src/orders/orders.route.js
@@ -20,7 +20,7 @@ router.get("/confirm-payment", async (req, res) => {
     if (order) {
         order.status = "processing";
         await order.save();
-        res.redirect(`http://1682-frontend.vercel.app`);
+        res.redirect(`http://localhost:5001`);
     } else {
         console.error("Order not found");
     }
